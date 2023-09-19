0eea7c4851fd9b5f70eb92fb580e01137563e642
Ira S <iryna.shevchenko2@gmail.com>
2023-09-19 11:00:16 +0300

    in pr

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -8,10 +8,10 @@ import { Section } from "../Section/Section"
 import { Statistics } from "../Statistics/Statistics"
 
 export class Counter extends React.Component {
-state = {
-  good: 0,
-  neutral: 0,
-  bad: 0
+    state = {
+        good: 0,
+        neutral: 0,
+        bad: 0
     }
 
     addGoodFeedback = () => {
@@ -41,54 +41,59 @@ state = {
         );
     };
 
-countTotalFeedback = () => {
-  const { good, neutral, bad } = this.state;
-  return good + neutral + bad;
-};
+    countTotalFeedback = () => {
+        const { good, neutral, bad } = this.state;
+        return good + neutral + bad;
+    };
 
-countPositiveFeedbackPercentage = () => {
-  const { good } = this.state;
-  const total = this.countTotalFeedback();
-  if (total === 0) {
-    return 0;
-  }
-  return Math.round((good / total) * 100);
-};
+    countPositiveFeedbackPercentage = () => {
+        const { good } = this.state;
+        const total = this.countTotalFeedback();
+        if (total === 0) {
+            return 0;
+        }
+        return Math.round((good / total) * 100);
+    };
 
-    
     render() {
         return (
             <div className={css.counter}>
                 <h1 className={css.firstHeadline}>Please leave feedback</h1>
-<div className={css.buttonsWrapper}>
-                    <button className={css.button}
-                        type="button"
-                        onClick={this.addGoodFeedback}>Good</button>
-                    <button className={css.button}
-                        type="button"
-                    onClick={this.addNeutralFeedback}>Neutral</button>
-                    <button className={css.button}
-                        type="button"
-                    onClick={this.addBadFeedback}>Bad</button>
-</div>
+                <div className={css.buttonsWrapper}>
+                    <button className={css.button} type="button" onClick={this.addGoodFeedback}>
+                        Good
+                    </button>
+                    <button className={css.button} type="button" onClick={this.addNeutralFeedback}>
+                        Neutral
+                    </button>
+                    <button className={css.button} type="button" onClick={this.addBadFeedback}>
+                        Bad
+                    </button>
+                </div>
                 <h2 className={css.secondHeadline}>Statistics</h2>
-
-                <span className={css.condition}>Good:</span>
-                <span className={css.amount}>{this.state.good}</span>
-
-                <span className={css.condition}>Neutral:</span>
-                <span className={css.amount}>{this.state.neutral}</span>
-
-                <span className={css.condition}>Bad:</span>
-                <span className={css.amount}>{this.state.bad}</span>
-
-                <span className={css.condition}>Total:</span>
-                <span className={css.amount}>{this.countTotalFeedback()}</span>
-
-                <span className={css.condition}>Positive feedback:</span>
-                <span className={css.amount}>{this.countPositiveFeedbackPercentage()}%</span>
-
+                <div className={css.spansWrapper}>
+                    <div className={css.spansElement}>
+                        <span className={css.condition}>Good:</span>
+                        <span className={css.amount}>{this.state.good}</span>
+                    </div>
+                    <div className={css.spansElement}>
+                        <span className={css.condition}>Neutral:</span>
+                        <span className={css.amount}>{this.state.neutral}</span>
+                    </div>
+                    <div className={css.spansElement}>
+                        <span className={css.condition}>Bad:</span>
+                        <span className={css.amount}>{this.state.bad}</span>
+                    </div>
+                    <div className={css.spansElement}>
+                        <span className={css.condition}>Total:</span>
+                        <span className={css.amount}>{this.countTotalFeedback()}</span>
+                    </div>
+                    <div className={css.spansElement}>
+                        <span className={css.condition}>Positive feedback:</span>
+                        <span className={css.amount}>{this.countPositiveFeedbackPercentage()}%</span>
+                    </div>
+                </div>
             </div>
         );
     }
-}
+};
